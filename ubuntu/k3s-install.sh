a9678550808c92fb4ea0f999dd1d331d2665e669
coutug <coutug18@hotmail.com>
2023-08-03 17:34:02 -0400

    add k3s install

--- ubuntu/k3s-install.sh
+++ ubuntu/k3s-install.sh
@@ -2,7 +2,7 @@
 set -e
 set -o noglob
 
-export INSTALL_K3S_EXEC="agent --prefer-bundled-bin --server https://10.220.118.2:6443 --token K10368065757c24fb40107267dbdc19e27930ea4d6a6840862bd8d1e63c6c02e73b::server:810e237c186dbdbe321d3d9d56d0c338"
+# export INSTALL_K3S_EXEC="agent --prefer-bundled-bin --server https://10.220.118.2:6443 --token K10368065757c24fb40107267dbdc19e27930ea4d6a6840862bd8d1e63c6c02e73b::server:810e237c186dbdbe321d3d9d56d0c338"
 
 # Usage:
 #   curl ... | ENV_VAR=... sh -
