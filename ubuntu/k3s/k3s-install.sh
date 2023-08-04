700b2dbe06f82e624cb859e12ab18ea68d9ae812
coutug <coutug18@hotmail.com>
2023-08-04 14:31:42 -0400

    add k3s scripts

--- ubuntu/k3s/k3s-install.sh
+++ ubuntu/k3s/k3s-install.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 set -ex
 
-export HOME=/root
 export DEBIAN_FRONTEND=noninteractive
 export INSTALL_K3S_SKIP_START=true
 
-systemctl stop unattended-upgrades
-apt-get -y purge unattended-upgrades
-
-apt-get update
-apt-get -y install \
-    git curl wget \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common \
-    conntrack \
-    jq vim nano emacs joe \
-    inotify-tools \
-    socat make golang-go \
-    unzip \
-    bash-completion \
-    dnsutils \
-    iputils-ping
-
 curl -sfL https://get.k3s.io | sh -
-
-mkdir -p /root/.kube
-ln -sf /etc/rancher/k3s/k3s.yaml /root/.kube/config
--- ubuntu/k3s/k3s-start.sh
+++ ubuntu/k3s/k3s-start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash +x
 
-INSTALL_K3S_EXEC="agent --server https://10.220.118.2:6443 --token K10368065757c24fb40107267dbdc19e27930ea4d6a6840862bd8d1e63c6c02e73b::server:810e237c186dbdbe321d3d9d56d0c338"
+K3S_URL="https://10.220.118.7:6443"
+K3S_TOKEN="token"
+INSTALL_K3S_EXEC="agent"
 
-/usr/local/bin/k3s $INSTALL_K3S_EXEC
+
+
+/usr/local/bin/k3s $K3S_URL $K3S_TOKEN $INSTALL_K3S_EXEC
