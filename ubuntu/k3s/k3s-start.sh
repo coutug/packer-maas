23885054ec097808cda1ad2a25ef76f5ea03baea
coutug <coutug18@hotmail.com>
2023-08-07 10:47:29 -0400

    add k3s scripts

--- ubuntu/k3s/k3s-start.sh
+++ ubuntu/k3s/k3s-start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash +x
 
-K3S_URL="https://10.220.118.7:6443"
-K3S_TOKEN="token"
-INSTALL_K3S_EXEC="agent"
+INSTALL_K3S_EXEC="agent --server https://10.220.118.7:6443 --token token"
 
-
-
-/usr/local/bin/k3s $K3S_URL $K3S_TOKEN $INSTALL_K3S_EXEC
+/usr/local/bin/k3s $INSTALL_K3S_EXEC
